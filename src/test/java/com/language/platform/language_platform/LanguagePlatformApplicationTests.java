0e208bfc7293e1c8c61bd21c21c561c584e18713
Natalya Brago <nata_9507@mail.ru>
2026-04-04 12:12:34 +0300

    first commit

---
+++ src/test/java/com/language/platform/language_platform/LanguagePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.language.platform.language_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguagePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
